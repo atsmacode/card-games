b35e26759fc77bd268ab23822d67452db144eb14
Mark Ramage <mramage@hotmail.co.uk>
2022-12-04 08:50:30 +0000

    Implement global DB connection

--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -2,15 +2,25 @@
 
 namespace Atsmacode\CardGames\Tests;
 
+use Atsmacode\CardGames\CardGamesConfigProvider;
+use Doctrine\DBAL\DriverManager;
 use PHPUnit\Framework\TestCase;
 
 abstract class BaseTest extends TestCase
 {
-
     protected function setUp(): void
     {
-        parent::setUp();
         $GLOBALS['THE_ROOT'] = '';
-        $GLOBALS['dev'] = true;
+        $GLOBALS['dev']      = true;
+        $config              = (new CardGamesConfigProvider)->get();
+        $env                 = 'test';
+
+        $GLOBALS['connection'] = DriverManager::getConnection([
+            'dbname'   => $config['db'][$env]['database'],
+            'user'     => $config['db'][$env]['username'],
+            'password' => $config['db'][$env]['password'],
+            'host'     => $config['db'][$env]['servername'],
+            'driver'   => $config['db'][$env]['driver'],
+        ]);
     }
 }
