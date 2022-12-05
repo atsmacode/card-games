476e4520cf14aeb4b8a23210a35c9a009472cb52
Mark Ramage <mramage@hotmail.co.uk>
2022-12-05 18:56:42 +0000

    Correct batch file command

--- dev/runtests.bat
+++ dev/runtests.bat
@@ -1,2 +1,2 @@
-php .\dev\SymfonyApplication.php app:build-card-games -d true
+C:\laragon\bin\php\php-8.1.3-nts-Win32-vs16-x64/php .\dev\CardGamesApp.php app:build-card-games -d true
 C:\laragon\bin\php\php-8.1.3-nts-Win32-vs16-x64/php ./vendor/bin/phpunit --testdox
