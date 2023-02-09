7c3e2c4a204d8c65ad0da586b5df99dfc1305b5a
Mark Ramage <mramage@hotmail.co.uk>
2023-02-09 18:16:30 +0000

    Reformat constant names - all caps

--- app/Constants/Rank.php
+++ app/Constants/Rank.php
@@ -4,106 +4,106 @@ namespace Atsmacode\CardGames\Constants;
 
 class Rank
 {
-    const ACE_rank_id   = 1;
-    const DEUCE_rank_id = 2;
-    const THREE_rank_id = 3;
-    const FOUR_rank_id  = 4;
-    const FIVE_rank_id  = 5;
-    const SIX_rank_id   = 6;
-    const SEVEN_rank_id = 7;
-    const EIGHT_rank_id = 8;
-    const NINE_rank_id  = 9;
-    const TEN_rank_id   = 10;
-    const JACK_rank_id  = 11;
-    const QUEEN_rank_id = 12;
-    const KING_rank_id  = 13;
+    const ACE_RANK_ID   = 1;
+    const DEUCE_RANK_ID = 2;
+    const THREE_RANK_ID = 3;
+    const FOUR_RANK_ID  = 4;
+    const FIVE_RANK_ID  = 5;
+    const SIX_RANK_ID   = 6;
+    const SEVEN_RANK_ID = 7;
+    const EIGHT_RANK_ID = 8;
+    const NINE_RANK_ID  = 9;
+    const TEN_RANK_ID   = 10;
+    const JACK_RANK_ID  = 11;
+    const QUEEN_RANK_ID = 12;
+    const KING_RANK_ID  = 13;
 
     const ACE = [
-        'rank_id'           => self::ACE_rank_id,
+        'rank_id'           => self::ACE_RANK_ID,
         'rank'              => 'Ace',
         'ranking'           => 1,
         'rankAbbreviation'  => 'A',
     ];
 
     const DEUCE = [
-        'rank_id'           => self::DEUCE_rank_id,
+        'rank_id'           => self::DEUCE_RANK_ID,
         'rank'              => 'Deuce',
         'ranking'           => 2,
         'rankAbbreviation'  => '2',
     ];
 
     const THREE = [
-        'rank_id'          => self::THREE_rank_id,
+        'rank_id'          => self::THREE_RANK_ID,
         'rank'             => 'Three',
         'ranking'          => 3,
         'rankAbbreviation' => '3',
     ];
 
     const FOUR = [
-        'rank_id'          => self::FOUR_rank_id,
+        'rank_id'          => self::FOUR_RANK_ID,
         'rank'             => 'Four',
         'ranking'          => 4,
         'rankAbbreviation' => '4',
     ];
 
     const FIVE = [
-        'rank_id'          => self::FIVE_rank_id,
+        'rank_id'          => self::FIVE_RANK_ID,
         'rank'             => 'Five',
         'ranking'          => 5,
         'rankAbbreviation' => '5',
     ];
 
     const SIX = [
-        'rank_id'          => self::SIX_rank_id,
+        'rank_id'          => self::SIX_RANK_ID,
         'rank'             => 'Six',
         'ranking'          => 6,
         'rankAbbreviation' => '6',
     ];
 
     const SEVEN = [
-        'rank_id'          => self::SEVEN_rank_id,
+        'rank_id'          => self::SEVEN_RANK_ID,
         'rank'             => 'Seven',
         'ranking'          => 7,
         'rankAbbreviation' => '7',
     ];
 
     const EIGHT = [
-        'rank_id'          => self::EIGHT_rank_id,
+        'rank_id'          => self::EIGHT_RANK_ID,
         'rank'             => 'Eight',
         'ranking'          => 8,
         'rankAbbreviation' => '8',
     ];
 
     const NINE = [
-        'rank_id'          => self::NINE_rank_id,
+        'rank_id'          => self::NINE_RANK_ID,
         'rank'             => 'Nine',
         'ranking'          => 9,
         'rankAbbreviation' => '9',
     ];
 
     const TEN = [
-        'rank_id'          => self::TEN_rank_id,
+        'rank_id'          => self::TEN_RANK_ID,
         'rank'             => 'Ten',
         'ranking'          => 10,
         'rankAbbreviation' => '10',
     ];
 
     const JACK = [
-        'rank_id'          => self::JACK_rank_id,
+        'rank_id'          => self::JACK_RANK_ID,
         'rank'             => 'Jack',
         'ranking'          => 11,
         'rankAbbreviation' => 'J',
     ];
 
     const QUEEN = [
-        'rank_id'          => self::QUEEN_rank_id,
+        'rank_id'          => self::QUEEN_RANK_ID,
         'rank'             => 'Queen',
         'ranking'          => 12,
         'rankAbbreviation' => 'Q',
     ];
 
     const KING = [
-        'rank_id'          => self::KING_rank_id,
+        'rank_id'          => self::KING_RANK_ID,
         'rank'             => 'King',
         'ranking'          => 13,
         'rankAbbreviation' => 'K',
--- app/Constants/Suit.php
+++ app/Constants/Suit.php
@@ -4,31 +4,31 @@ namespace Atsmacode\CardGames\Constants;
 
 class Suit
 {
-    const CLUBS_suit_id     = 1;
-    const DIAMONDS_suit_id  = 2;
-    const HEARTS_suit_id    = 3;
-    const SPADES_suit_id    = 4;
+    const CLUBS_SUIT_ID     = 1;
+    const DIAMONDS_SUID_ID = 2;
+    const HEARTS_SUIT_ID   = 3;
+    const SPADES_SUIT_ID   = 4;
 
     const CLUBS = [
-        'suit_id'          => self::CLUBS_suit_id,
+        'suit_id'          => self::CLUBS_SUIT_ID,
         'suit'             => 'Clubs',
         'suitAbbreviation' => 'C',
     ];
 
     const DIAMONDS = [
-        'suit_id'          => self::DIAMONDS_suit_id,
+        'suit_id'          => self::DIAMONDS_SUID_ID,
         'suit'             => 'Diamonds',
         'suitAbbreviation' => 'D',
     ];
 
     const HEARTS = [
-        'suit_id'          => self::HEARTS_suit_id,
+        'suit_id'          => self::HEARTS_SUIT_ID,
         'suit'             => 'Hearts',
         'suitAbbreviation' => 'H',
     ];
 
     const SPADES = [
-        'suit_id'          => self::SPADES_suit_id,
+        'suit_id'          => self::SPADES_SUIT_ID,
         'suit'             => 'Spades',
         'suitAbbreviation' => 'S',
     ];
